5960de8542bda0644fe71101e16f587f645f690f
ValentinaAltamirano <107081335+ValentinaAltamirano@users.noreply.github.com>
2022-09-28 17:01:28 -0300

    Update expocarrera.py

--- expocarrera.py
+++ expocarrera.py
@@ -1,257 +1,253 @@
-# Escribir una aplicación GUI (llamada ContDecreciente) como la que se
-# ve en la figura. Cada ves que se haga clic en el botón "-", al valor de
-# contador se le resta 1.
-
-from asyncio.windows_events import NULL
-from cProfile import label
 from tkinter import *
-import tkinter as tk
-from tkinter.font import Font
 
-class Juego(tk.Tk):
+def ventanaCreate(v):
+    v.title("Adivinador de numero") 
+    v.geometry("1366x700")
+    v.resizable(width=False, height=False)
+    v.configure(bg="#2c2638")
 
-    def __init__(self):
-        super().__init__()
-        self.title("Adivinador")
-        self.resizable(False, False)
-        self.geometry("550x300")
-        self.columnconfigure(0, weight=1)
-        self.rowconfigure(0, weight=1)
-        self.txt = StringVar()
-        self.img = PhotoImage(file="emoji.png", width=100, height=83)
-        self.btnFont = Font(family="Roboto Cn", size=10) 
-        self.txtFont = Font(family="Roboto Cn", size=14) 
-        self.cont = 0
-        self.mayMen = ''
-        
-        self.texto = Label()
-        self.resta = Label()
-        self.suma = Label()
-        self.btnReset = Label()
-        self.btnAtras = Label()
-        self.btnSig = Label()
-        self.mayor = Label()
-        self.menor = Label()
-        self.Idioma()
-        
-    def changeEspañol(self):
-        print(self.cont)
-        self.cont += 1
-        if self.cont == 1:
-
-            self.txt.set("¡Bienvenido al juego! \n\n Por favor, piensa en un número de dos cifras \n que no sean iguales.")
-            self.texto.place(x=76, y=76)
-            self.btnSig.destroy()
-            self.btnSig = Button(self, text="Siguiente",command=lambda: self.changeEspañol())
-            self.btnSig.config(font=self.btnFont, fg="black", bg="white", border= 1)
-            self.btnSig.place(x=280, y=200)
-            self.btnAtras.destroy()
-            self.btnAtras = Button(self, text="Anterior",command=lambda: self.Idioma())
-            self.btnAtras.config(font=self.btnFont, fg="black", bg="white", border= 1)
-            self.btnAtras.place(x=205, y=200)
-            self.mayor.destroy()
-            self.menor.destroy()
-            self.ing.destroy()
-            self.esp.destroy()
-            self.btnReset.destroy()
-
-            
-        if self.cont == 2:
-            self.radioValue = tk.IntVar() 
-
-            self.txt.set("\n\n Bien, ahora invertí el orden de las cifras. \n Luego, ingrese si el numero es mayor or menor")
-            self.texto.place(x=77, y=26)
-
-            self.mayor = Radiobutton(self, text="Mayor",variable=self.radioValue, value=1)
-            self.mayor.config(font=self.btnFont, fg="black", border= 1)
-            self.mayor.place(x=247, y=130)
-
-            self.menor = Radiobutton(self, text="Menor",variable=self.radioValue, value=2)
-            self.menor.config(font=self.btnFont, fg="black", border= 1)
-            self.menor.place(x=247, y=160)
-            
-            self.btnAtras.destroy()
-            self.btnAtras = Button(self, text="Anterior",command=lambda: self.atras())
-            self.btnAtras.config(font=self.btnFont, fg="black", bg="white", border= 1)
-            self.btnAtras.place(x=205, y=200)
-            self.btnSig.place(x=280, y=200)
-            
-            self.resta.destroy()
-
-        if self.cont == 3:
-            self.restaValue = tk.IntVar() 
-
-            self.txt.set("\n\n Ahora, al numero mayor de los dos restale el menor. \n Ingrese el resultado:")
-            self.texto.place(x=55, y=26)
-            self.mayor.destroy()
-            self.menor.destroy()
-            self.resta = Entry(self,textvariable=self.restaValue)
-            self.resta.place(x=213, y=150)
-
-            self.suma.destroy()
-
-        if self.cont == 4:
-            self.sumaValue = tk.IntVar() 
-
-            self.txt.set("\n\n Luego sumá las cifras del número \nque pensaste al principio. \n Ingrese el resultado:")
-            self.texto.place(x=135, y=13)
-            self.resta.destroy()
-            self.suma = Entry(self,textvariable=self.sumaValue)
-            self.suma.place(x=213, y=150)
-            self.btnReset.destroy()
-
-        if self.cont == 5:
-            n = self.radioValue.get()
-            self.btnSig.destroy()
-
-            if n == 1:
-
-                self.btnReset = Button(self, text="Empezar de nuevo",command=lambda: self.reset())
-                self.texto.place(x=176, y=80)
-                self.btnReset.config(font=self.btnFont, fg="black", bg="white", border= 1)
-                self.btnReset.place(x=252, y=180)
-                self.btnAtras.place(x=178, y=180)
-
-                
-                num1 = int(self.restaValue.get())
-                num2 = int(self.suma.get())
-                rta = self.calcular(num1, num2)
-                self.txt.set("Pensaste en el numero: \n\n" + str(rta))
-                self.suma.destroy()
-
-                
-    def calcular(self, n1, n2):
-
-        parametro = n1 / 9
-
-        d1 = (n2 - parametro) / 2
-        d2 = (n2 + parametro) / 2
-
-        respuesta = int(d1) * 10 + int(d2)
-
-        return respuesta
-
-   
-    def atras(self):
-        self.cont -= 2
-        self.changeEspañol()
-
-    def reset(self):
-        self.cont = 0
-        self.changeEspañol()
-
+def atras(var1, var2):
+    var1.withdraw()
+    var2.deiconify()
     
-    def changeIngles(self):
+def paso1():
+    global v1,radioVal
+    v1 = Tk()
+    ventanaCreate(v1)
+    
+    texto = Label(v1,text="¡Bienvenido! \n\n Por favor ingrese el idioma en el que desea jugar:")
+    texto.config(fg="#ffa7c4", bg='#2c2638',font=("Verdana",30))
+    texto.place(x=181, y=165)
 
-        self.cont += 1
-        if self.cont == 1:
+    radioVal = IntVar() 
 
-            self.txt.set("¡Welcome to the game! \n\n Please think on a two digit number,  \n each one has to be different.")
-            self.texto.place(x=76, y=76)
-            self.btnSig = Button(self, text="Continue",command=lambda: self.changeIngles())
-            self.btnSig.config(font=self.btnFont, fg="black", bg="white", border= 1)
-            self.btnSig.place(x=280, y=200)
-            self.btnAtras.destroy()
-            self.btnAtras = Button(self, text="Previous",command=lambda: self.Idioma())
-            self.btnAtras.config(font=self.btnFont, fg="black", bg="white", border= 1)
-            self.btnAtras.place(x=205, y=200)
-            self.mayor.destroy()
-            self.menor.destroy()
-            self.ing.destroy()
-            self.esp.destroy()
-            self.btnReset.destroy()
-            
-        if self.cont == 2:
-            self.radioValue = tk.IntVar() 
+    esp = Radiobutton(v1, text="Español",variable=radioVal, value=1, command=lambda: paso2())
+    esp.config(fg="#ffa7c4", bg='#353042',font=("Verdana",30),width = 20)
+    esp.place(x=412, y=370)
 
-            self.txt.set("\n\n Now revert the oder of the digits. \n Then, enter if the number is bigger or smaller.")
-            self.texto.place(x=77, y=26)
+    ing = Radiobutton(v1, text="Inglés",variable=radioVal, value=2, command=lambda: paso2())
+    ing.config(fg="#ffa7c4", bg='#353042',font=("Verdana",30),width = 20)
+    ing.place(x=412, y=460)
 
-            self.mayor = Radiobutton(self, text="Bigger",variable=self.radioValue, value=1)
-            self.mayor.config(font=self.btnFont, fg="black", border= 1)
-            self.mayor.place(x=247, y=130)
+    v1.mainloop()
 
-            self.menor = Radiobutton(self, text="Smaller",variable=self.radioValue, value=2)
-            self.menor.config(font=self.btnFont, fg="black", border= 1)
-            self.menor.place(x=247, y=160)
-            
-            self.btnAtras.destroy()
-            self.btnAtras = Button(self, text="Previous",command=lambda: self.atrasIngles())
-            self.btnAtras.config(font=self.btnFont, fg="black", bg="white", border= 1)
-            self.btnAtras.place(x=205, y=200)
-            self.btnSig.place(x=280, y=200)
-            
-            self.resta.destroy()
+def paso2():
+    global v2
+    v1.withdraw()
+    v2 = Tk()
+    ventanaCreate(v2)
 
-        if self.cont == 3:
-            self.restaValue = tk.IntVar() 
+    if radioVal.get() == 1:
+        texto = Label(v2,text="Por favor, piensa en un número entero de\n dos cifras, que no sean iguales.\n\n ¿Listo?")
+        texto.place(x=263, y=171)
+        texto.config(fg="#ffa7c4", bg='#2c2638',font=("Verdana",30))
 
-            self.txt.set("\n\n To the biggest number subtract the smallest. \n Enter the result:")
-            self.texto.place(x=55, y=26)
-            self.mayor.destroy()
-            self.menor.destroy()
-            self.resta = Entry(self,textvariable=self.restaValue)
-            self.resta.place(x=213, y=150)
+        cont = Button(v2, text="Continuar", command=lambda: paso3())
+        cont.config(fg="#353042", bg='#ffa7c4',font=("Verdana",40),width = 10, border = 0)
+        cont.place(x=715, y=418)
 
-            self.suma.destroy()
+        volver = Button(v2, text="Volver", command=lambda: atras(v2, v1))
+        volver.config(fg="#353042", bg='#ffa7c4',font=("Verdana",40),width = 10, border = 0)
+        volver.place(x=313, y=418)
 
-        if self.cont == 4:
-            self.sumaValue = tk.IntVar() 
+    elif radioVal.get() == 2:
+        texto = Label(v2,text="Think of a two-digit number (not equal). \n\n Done?")
+        texto.place(x=273, y=160)
+        texto.config(fg="#ffa7c4", bg='#2c2638',font=("Verdana",30))
 
-            self.txt.set("\n\n Now sum the digits of the \n first number you thought. \n Enter the result:")
-            self.texto.place(x=135, y=13)
-            self.resta.destroy()
-            self.suma = Entry(self,textvariable=self.sumaValue)
-            self.suma.place(x=213, y=150)
-            self.btnReset.destroy()
+        cont = Button(v2, text="Continue", command=lambda: paso3())
+        cont.config(fg="#353042", bg='#ffa7c4',font=("Verdana",40),width = 10, border = 0)
+        cont.place(x=715, y=418)
 
-        if self.cont == 5:
-            n = self.radioValue.get()
-            self.btnSig.destroy()
+        volver = Button(v2, text="Return", command=lambda: atras(v2, v1))
+        volver.config(fg="#353042", bg='#ffa7c4',font=("Verdana",40),width = 10, border = 0)
+        volver.place(x=313, y=418)
+    
+    v2.mainloop()
 
-            if n == 1:
+def paso3():
 
-                self.btnReset = Button(self, text="Start again",command=lambda: self.resetIngles())
-                self.texto.place(x=176, y=80)
-                self.btnReset.config(font=self.btnFont, fg="black", bg="white", border= 1)
-                self.btnReset.place(x=252, y=180)
-                self.btnAtras.place(x=178, y=180)
+    global v3, mayorMenor
+    v2.withdraw()
 
-                
-                num1 = int(self.restaValue.get())
-                num2 = int(self.suma.get())
-                rta = self.calcular(num1, num2)
-                self.txt.set("The number you thought is: \n\n" + str(rta))
-                self.suma.destroy() 
+    v3 = Tk()
+    ventanaCreate(v3)
 
-    def atrasIngles(self):
-        self.cont -= 1
-        self.changeIngles()
+    mayorMenor= IntVar(v3, "")
 
-    def resetIngles(self):
-        self.cont = 0
-        self.changeIngles()
+    if radioVal.get() == 1:
+        texto = Label(v3,text="Bien, ahora invertí el orden de las cifras.\n\n Luego, ingrese si el numero es mayor or menor")
+        texto.place(x=151, y=80)
+        texto.config(fg="#ffa7c4", bg='#2c2638',font=("Verdana",30))
 
-    def Idioma(self):
-        
-        self.radioVal = tk.IntVar() 
+        mayor = Radiobutton(v3, text="Mayor",variable=mayorMenor, value=1, command=lambda: paso4())
+        mayor.config(fg="#ffa7c4", bg='#353042',font=("Verdana",30),width = 20)
+        mayor.place(x=412, y=283)
 
-        self.txt.set("\n\n Bienvenido! Por favor ingrese el idioma \n en el que desea jugar:")
-        self.texto.destroy()
-        self.texto = Label(self)
-        self.texto.config(font =self.txtFont,textvariable=self.txt)
-        self.texto.place(x=110, y=20)
+        menor = Radiobutton(v3, text="Menor",variable=mayorMenor, value=2, command=lambda: paso4())
+        menor.config(fg="#ffa7c4", bg='#353042',font=("Verdana",30),width = 20)
+        menor.place(x=412, y=372)
 
-        self.esp = Radiobutton(self, text="Español",variable=self.radioVal, value=1,command=lambda: self.changeEspañol())
-        self.esp.config(font=self.btnFont, fg="black", border= 1)
-        self.esp.place(x=247, y=130)
+        volver = Button(v3, text="Volver", command=lambda: atras(v3, v2))
+        volver.config(fg="#353042", bg='#ffa7c4',font=("Verdana",40),width = 10, border = 0)
+        volver.place(x=517, y=500)
 
-        self.ing = Radiobutton(self, text="Inglés",variable=self.radioVal, value=2,command=lambda: self.changeIngles())
-        self.ing.config(font=self.btnFont, fg="black", border= 1)
-        self.ing.place(x=247, y=160)
+    elif radioVal.get() == 2:
+        texto = Label(v3,text="Now reverse the order of the figures. \n Then, enter if the new number is \n higher or lower than the first")
+        texto.place(x=310, y=80)
+        texto.config(fg="#ffa7c4", bg='#2c2638',font=("Verdana",30))
 
-        self.btnAtras.destroy()
-        self.btnSig.destroy()
-        
-root = Juego()
-root.mainloop()
+        mayor = Radiobutton(v3, text="Higher",variable=mayorMenor, value=1, command=lambda: paso4())
+        mayor.config(fg="#ffa7c4", bg='#353042',font=("Verdana",30),width = 20)
+        mayor.place(x=412, y=283)
+
+        menor = Radiobutton(v3, text="Lower",variable=mayorMenor, value=2,command=lambda: paso4())
+        menor.config(fg="#ffa7c4", bg='#353042',font=("Verdana",30),width = 20)
+        menor.place(x=412, y=372)
+
+        volver = Button(v3, text="Return", command=lambda: atras(v3, v2))
+        volver.config(fg="#353042", bg='#ffa7c4',font=("Verdana",40),width = 10, border = 0)
+        volver.place(x=517, y=500)
+    
+    v3.mainloop()
+
+def paso4():
+    global v4, restaValue
+    v3.withdraw()
+    v4 = Tk()
+    ventanaCreate(v4)
+    restaValue = IntVar(v4, 0)
+
+    if radioVal.get() == 1:
+        texto = Label(v4,text="Ahora, al numero mayor restale el menor. \n\n Ingrese el resultado:")
+        texto.place(x=261, y=80)
+        texto.config(fg="#ffa7c4", bg='#2c2638',font=("Verdana",30))
+
+        resta = Entry(v4,textvariable=restaValue)
+        resta.config(fg="#2c2638", bg='#ffa7c4',font=("Verdana",30))
+        resta.place(x=432, y=298)
+
+        cont = Button(v4, text="Continuar", command=lambda: paso5())
+        cont.config(fg="#353042", bg='#ffa7c4',font=("Verdana",40),width = 10, border = 0)
+        cont.place(x=715, y=418)
+
+        volver = Button(v4, text="Volver", command=lambda: atras(v4, v3))
+        volver.config(fg="#353042", bg='#ffa7c4',font=("Verdana",40),width = 10, border = 0)
+        volver.place(x=313, y=418)
+    
+    if radioVal.get() == 2:
+        texto = Label(v4,text="To the higher number subtract the lower one. \n\n Enter the result:")
+        texto.place(x=225, y=80)
+        texto.config(fg="#ffa7c4", bg='#2c2638',font=("Verdana",30))
+
+        resta = Entry(v4,textvariable=restaValue)
+        resta.config(fg="#2c2638", bg='#ffa7c4',font=("Verdana",30))
+        resta.place(x=432, y=298)
+
+        cont = Button(v4, text="Continue", command=lambda: paso5())
+        cont.config(fg="#353042", bg='#ffa7c4',font=("Verdana",40),width = 10, border = 0)
+        cont.place(x=715, y=418)
+
+        volver = Button(v4, text="Return", command=lambda: atras(v4, v3))
+        volver.config(fg="#353042", bg='#ffa7c4',font=("Verdana",40),width = 10, border = 0)
+        volver.place(x=313, y=418)
+
+def paso5():
+    global v5, sumaValue
+    v4.withdraw()
+    v5 = Tk()
+    ventanaCreate(v5)
+    sumaValue = IntVar(v5, 0)
+
+    if radioVal.get() == 1:
+        texto = Label(v5,text="Luego sumá las cifras del número \nque pensaste al principio. \n\n Ingrese el resultado:")
+        texto.place(x=349, y=80)
+        texto.config(fg="#ffa7c4", bg='#2c2638',font=("Verdana",30))
+
+        resta = Entry(v5,textvariable=sumaValue)
+        resta.config(fg="#2c2638", bg='#ffa7c4',font=("Verdana",30))
+        resta.place(x=432, y=298)
+
+        cont = Button(v5, text="Continuar", command=lambda: paso6())
+        cont.config(fg="#353042", bg='#ffa7c4',font=("Verdana",40),width = 10, border = 0)
+        cont.place(x=715, y=418)
+
+        volver = Button(v5, text="Volver", command=lambda: atras(v5, v4))
+        volver.config(fg="#353042", bg='#ffa7c4',font=("Verdana",40),width = 10, border = 0)
+        volver.place(x=313, y=418)
+    
+    if radioVal.get() == 2:
+        texto = Label(v5,text="Now add the digits of the number \n you thought at the beginning. \n\n Enter the result? ")
+        texto.place(x=349, y=70)
+        texto.config(fg="#ffa7c4", bg='#2c2638',font=("Verdana",30))
+
+        resta = Entry(v5,textvariable=sumaValue)
+        resta.config(fg="#2c2638", bg='#ffa7c4',font=("Verdana",30))
+        resta.place(x=432, y=298)
+
+        cont = Button(v5, text="Continue", command=lambda: paso6())
+        cont.config(fg="#353042", bg='#ffa7c4',font=("Verdana",40),width = 10, border = 0)
+        cont.place(x=715, y=418)
+
+        volver = Button(v5, text="Return", command=lambda: atras(v5, v4))
+        volver.config(fg="#353042", bg='#ffa7c4',font=("Verdana",40),width = 10, border = 0)
+        volver.place(x=313, y=418)
+
+def paso6():
+    global v6
+    v5.withdraw()
+    v6 = Tk()
+    ventanaCreate(v6)
+    txt = StringVar(v6, "")
+
+    num1 = int(restaValue.get())
+    num2 = int(sumaValue.get())
+    rta = calcular(num1, num2)
+
+    if radioVal.get() == 1:
+        txt.set("Pensaste en el numero: \n\n" + str(rta))
+        texto = Label(v6,textvariable=txt)
+        texto.place(x=349, y=120)
+
+        texto.config(fg="#ffa7c4", bg='#2c2638',font=("Verdana",40))
+        cont = Button(v6, text="Volver a jugar", command=lambda: atras(v6, v1))
+        cont.config(fg="#353042", bg='#ffa7c4',font=("Verdana",40),width = 15, border = 0)
+        cont.place(x=633, y=418)
+
+        volver = Button(v6, text="Volver", command=lambda: atras(v6, v5))
+        volver.config(fg="#353042", bg='#ffa7c4',font=("Verdana",40),width = 10, border = 0)
+        volver.place(x=229, y=418)
+    
+    
+    if radioVal.get() == 2:
+        txt.set("You thought on the number: \n\n" + str(rta))
+        texto = Label(v6,textvariable=txt)
+        texto.place(x=303, y=120)
+        texto.config(fg="#ffa7c4", bg='#2c2638',font=("Verdana",40))
+    
+        cont = Button(v6, text="Restart", command=lambda: atras(v6, v1))
+        cont.config(fg="#353042", bg='#ffa7c4',font=("Verdana",40),width = 10, border = 0)
+        cont.place(x=715, y=418)
+
+        volver = Button(v6, text="Return", command=lambda: atras(v6, v5))
+        volver.config(fg="#353042", bg='#ffa7c4',font=("Verdana",40),width = 10, border = 0)
+        volver.place(x=313, y=418)
+
+
+def calcular(n1, n2):
+
+    parametro = n1 / 9
+
+    if mayorMenor.get() == 1:
+         d1 = (n2 - parametro) / 2
+         d2 = (n2 + parametro) / 2
+         respuesta = int(d1) * 10 + int(d2)
+    
+    if mayorMenor.get() == 2:
+         d1 = (n2 - parametro) / 2
+         d2 = (n2 + parametro) / 2
+         respuesta = int(d2) * 10 + int(d1)
+
+    return respuesta
+
+paso1()
